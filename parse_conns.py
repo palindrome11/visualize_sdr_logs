806cb892f2b819478bca8e6fd20118e39b60c99e
Christopher Conlon <cwconlon@gmail.com>
2021-12-16 16:17:20 -0800

    Database routines for update added

--- parse_conns.py
+++ parse_conns.py
@@ -1,7 +1,55 @@
 import datetime
 import os
 import itertools
+import sqlite3
 
+MHASH = {
+    "Jan": 1,
+    "Feb": 2,
+    "Mar": 3,
+    "Apr": 4,
+    "May": 5,
+    "Jun": 6,
+    "Jul": 7,
+    "Aug": 8,
+    "Sep": 9,
+    "Oct": 10,
+    "Nov": 11,
+    "Dec": 12
+}
+
+def db_create(db_name):
+
+    db = sqlite3.connect(db_name)
+
+    db.executescript("""
+    
+    drop table if exists connections;
+    create table connections (
+      id integer primary key autoincrement,
+      datetime_julian real not null,
+      connections integer);
+     """)
+
+    db.close()
+
+def db_update_record(db_name, db_record_update):
+    db = sqlite3.connect(db_name)
+    cursor = db.cursor()
+    print("Successfully Connected to {} database",format(db_name))
+
+    sqllite_insert_with_param = ("""
+    INSERT INTO connections (datetime_julian, connections) VALUES (?, ?);
+    """)
+    data_tuple = (db_record_update[0],db_record_update[1])
+    cursor.execute(sqllite_insert_with_param, data_tuple)
+    db.commit()
+
+    cursor.close()
+    db.close()
+
+# This was an older routine that seem to have been ost in space. Need to revisit and possibly reuse the delta time
+# logic
 def interval_proc(series):
     interval_series=[]
     for line in fh:
@@ -17,6 +65,11 @@ def interval_proc(series):
         intervel_series = interval_series.append(interval_list)
         print(delta_time)
 
+def month_to_num(Month):
+    Month_num = MHASH[Month]
+    return(Month_num)
+
+
 def split_clock_time(Time):
     ctime=[]
     ctime = Time.split(":")
@@ -27,50 +80,68 @@ def split_clock_time(Time):
     return([Hour,Minute,Seconds])
 
 def collate(Month,Day,Time,Year,Connections):
+    Month_Num=month_to_num(Month)
     Hours, Minutes, Seconds = split_clock_time(Time)
-    print("Month: {}\nDay: {}\nYear: {}\nHours: {}\nMinutes: {}\nSeconds: {}\nConnections: {}\n\n ".format(Month,Day,Year,Hours,Minutes,Seconds,Connections))
+    jtime=datetime.datetime(int(Year),int(Month_Num),int(Day),int(Hours),int(Minutes),int(Seconds)).timestamp()
+    print("Month: {}\nDay: {}\nYear: {}\nHours: {}\nMinutes: {}\nSeconds: {}\nConnections: {}\nJulian Time(UTC): {}\n\n ".format(Month,Day,Year,Hours,Minutes,Seconds,Connections,jtime))
+    record = {"Month" : Month_Num,
+              "Day" : Day,
+              "Year" : Year,
+              "Hours" : Hours,
+              "Minutes" : Minutes,
+              "Seconds" : Seconds,
+              "jtime" : jtime,
+              "Connections" : Connections
+    }
+    return(record)
+
+def parse_file(filename, db_name):
+
+    # sdr.stats:SDR Stats at ---- ----Mon Dec 13 00:05:06 2021
+    # 0 sdr.stats:SDR
+    # 1 Stats
+    # 2 at
+    # 3 ----
+    # 4 ----Mon
+    # 5 Dec
+    # 6 13
+    # 7 00:05:06
+    # 8 2021
 
 
-# sdr.stats:SDR Stats at ---- ----Mon Dec 13 00:05:06 2021
-# 0 sdr.stats:SDR
-# 1 Stats
-# 2 at
-# 3 ----
-# 4 ----Mon
-# 5 Dec
-# 6 13
-# 7 00:05:06
-# 8 2021
+    with open(filename, "r+") as fh:
+        el1=[]
+        el2=[]
+        for line in fh:
+            line=line.strip()
+            el1=line.split(" ")
+            try:
+                el1.remove("")
+            except:
+                pass
+    #        print(el1)
+            Month=(el1[5])
+    #        print('Month: {}'.format(Month))
+            Day=(el1[6])
+    #        print('Day: {}'.format(Day))
+            Time=(el1[7])
+    #        print('Time: {}'.format(Time))
+            Year=(el1[8])
+    #        print('Year: {}'.format(Year))
+    #        exit()
+            line=next(fh)
+            line=line.strip()
+            el2=line.split(" ")
+            Connections=(el2[1])
+            record = collate(Month,Day,Time,Year,Connections)
+#            print(record['Day'])
+            jdate_time = record['jtime']
+            connect_no = record['Connections']
+            db_record_update = [jdate_time,int(connect_no)]
+            db_update_record(db_name,db_record_update)
 
 
-with open("conn_histry.txt", "r+") as fh:
-    el1=[]
-    el2=[]
-    for line in fh:
-        line=line.strip()
-        el1=line.split(" ")
-        try:
-            el1.remove("")
-        except:
-            pass
-#        print(el1)
-        Month=(el1[5])
-#        print('Month: {}'.format(Month))
-        Day=(el1[6])
-#        print('Day: {}'.format(Day))
-        Time=(el1[7])
-#        print('Time: {}'.format(Time))
-        Year=(el1[8])
-#        print('Year: {}'.format(Year))
-#        exit()
-        line=next(fh)
-        line=line.strip()
-        el2=line.split(" ")
-        Connections=(el2[1])
-        collate(Month,Day,Time,Year,Connections)
-
-
-
-
+db_create("connections.db")
+parse_file("conn_histry.txt", "connections.db")
 
 
